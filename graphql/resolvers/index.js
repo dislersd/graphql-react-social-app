1757bfd17bb4f5fe99cd6fdd4c9011f18df42bd3
dislersd <dislersd@gmail.com>
2019-07-07 13:22:05 -0700

    added like and comment counts

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,18 +1,21 @@
-const postsResolvers = require('./posts');
-const usersResolvers = require('./users');
-const commentsResolvers = require('./comments');
+const postsResolvers = require("./posts");
+const usersResolvers = require("./users");
+const commentsResolvers = require("./comments");
 
 module.exports = {
-    Query: {
-        ...postsResolvers.Query
-    },
-    Mutation: {
-        ...usersResolvers.Mutation,
-        ...postsResolvers.Mutation,
-        ...commentsResolvers.Mutation
-
-    },
-    Subscription: {
-        ...postsResolvers.Subscription
-    }
-}
+  Post: {
+    likeCount: parent => parent.likes.length,
+    commentCount: parent => parent.comments.length
+  },
+  Query: {
+    ...postsResolvers.Query
+  },
+  Mutation: {
+    ...usersResolvers.Mutation,
+    ...postsResolvers.Mutation,
+    ...commentsResolvers.Mutation
+  },
+  Subscription: {
+    ...postsResolvers.Subscription
+  }
+};
