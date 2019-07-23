2d6f396192f33ab6278ee357ad571fc7c2d1b83f
dislersd <dislersd@gmail.com>
2019-07-23 06:04:01 -0700

    update

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
-const { MONGODB } = require("./config");
 
 const pubsub = new PubSub();
 
